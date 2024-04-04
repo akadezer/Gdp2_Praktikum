05381126f95585e1cc76b73f0ec115cc2435e6c5
Aleks M <aleksandarmaksimovic257@gmail.com>
2024-04-04 17:19:20 +0200

    Start of project

---
+++ va06/src/AudioFile.java
@@ -0,0 +1,23 @@
+public class AudioFile{
+	
+	private String pathname; 
+	private String filename;
+	
+	public AudioFile() {
+		
+	}
+	
+	public void parsePathname(String path){
+		
+		
+	}
+	
+	public String getPathname(){
+		
+		return pathname;
+	}
+	
+	public String getFilename() {
+		return filename;
+	}
+}
