8f34c114629c18fb841317e03f46977f36f6f76c
Aleks M <aleksandarmaksimovic257@gmail.com>
2024-04-06 19:34:16 +0200

    implemented parsePathname & parseFilename

--- va06/src/AudioFile.java
+++ va06/src/AudioFile.java
@@ -1,13 +1,21 @@
-import java.awt.desktop.SystemSleepEvent;
+
 
 public class AudioFile{
 
 
-	private String pathname; 
-	private String filename;
-	
-	public AudioFile() {
-		
+	private String pathname; // Pfadname
+	private String filename; // Dateiname
+	private String author; // Autor
+	private String title; // Titel
+
+
+	public AudioFile(){
+
+	}
+	public AudioFile(String path) {
+		parsePathname(path);
+		parseFilename(filename);
+
 	}
 	
 	public void parsePathname(String path){
@@ -17,7 +25,18 @@ public class AudioFile{
 		}else {
 			sep = Utils.emulateLinux();
 		}
-		// Laufwerksangabe 
+		//Laufwerkskorrektur
+		if (!isWindows()){
+
+		if (path.contains(":")){
+
+			path = sep + path.substring(0,path.indexOf(":")) + path.substring(path.indexOf(":")+1);
+		}
+
+
+		}
+
+
 
 	// Testet ob der Pfadseperator öfter vorkommt
 		for (int i = 0; i < path.length(); i++){
@@ -26,16 +45,63 @@ public class AudioFile{
 				path = path.substring(0,(i+1)) + path.substring(i+2);
 			}
 		}
+
+
+		pathname = path;
+		if (path.lastIndexOf(sep)){
+
+			filename =  path.substring(path.lastIndexOf(sep)+1);
+		}
+
 	}
-	
+
+	public void parseFilename(String filename){
+
+		filename = 	filename.substring(0,filename.lastIndexOf("."));
+		if (filename.contains(" - ")){
+		// Trennung zwischen Autor und Titel
+	String[] fileparts = filename.split(" - ");
+	author = fileparts[0].trim();
+	title = fileparts[1].trim();
+		}else {
+			title = filename;
+		}
+	if (filename.equals("-") ){
+		author = "";
+		title = "-";
+	}
+
+
+	}
+
+	//Getter-Methoden
 	public String getPathname(){
 		
 		return pathname;
+
 	}
 	
 	public String getFilename() {
 		return filename;
 	}
+
+	public String getAuthor(){
+		return author;
+	}
+
+	public String getTitle(){
+		return title;
+	}
+
+	@Override
+	public String toString() {
+		if (getAuthor().equals("")){
+			return getTitle();
+		}else{
+		return getAuthor() + " - " + getTitle();}
+	}
+
+	// Systemabfrage
 private boolean isWindows() {
 	return System.getProperty("os.name").toLowerCase().indexOf("win") >= 0;
 }
